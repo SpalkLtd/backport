3b44a042a8ab3f7e2e8880e0d95c0ed3eb37bf34
alcamerone <al.camerone@gmail.com>
2017-04-05 15:43:18 +1200

    Added 'null' channel

---
+++ src/Notifications/Channels/NullChannel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Illuminate\Notifications\Channels;
+
+use Illuminate\Notifications\Notification;
+
+class NullChannel
+{
+    public function __construct()
+    {
+        // 
+    }
+
+    /**
+     * Send the given notification.
+     *
+     * @param mixed $notifiable
+     * @param \Illuminate\Notifications\Notification $notification
+     *
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        // 
+    }
+}
